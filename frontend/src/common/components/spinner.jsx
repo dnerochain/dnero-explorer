656d98e7bcaea9e23b0b6338eae389616d76ebec
Muroko <murokomail@gmail.com>
2023-10-16 12:34:40 -0400

    REMOVE: frontend
    separating backend and frontend

--- frontend/src/common/components/spinner.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import cx from 'classnames';
-
-export default function Spinner({ className, ...props }) {
-  return (
-    <div className={cx("spinner", className)} {...props}></div>);
-}
